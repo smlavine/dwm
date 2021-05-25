bde6aacef32ffb3e77da1c9b791abcb395780476
Sebastian LaVine <mail@smlavine.com>
2021-05-25 07:26:01 -0400

    Add keybinding to close most recent dunst notification
    TODO: Find a way to bind to Keysym, not KeyCode, or to somehow otherwise
    recognize that I have CapsLock bound to Escape, so that I don't need to
    define the binding for both keys.

--- config.h
+++ config.h
@@ -125,6 +125,8 @@ static Key keys[] = {
     { MODKEY,                       XK_y,      spawn,          SHCMD("lastscreenshot") },
     { MODKEY|ShiftMask,             XK_BackSpace, spawn,       SHCMD("quitdwm") },
     { MODKEY,                       XK_End,    spawn,          SHCMD("sudo light -U 5") },
+    { MODKEY,                       XK_Escape, spawn,          SHCMD("dunstctl close") },
+    { MODKEY,                       XK_Caps_Lock, spawn,       SHCMD("dunstctl close") },
     { MODKEY,                       XK_Home,   spawn,          SHCMD("sudo light -A 5") },
 };
 
